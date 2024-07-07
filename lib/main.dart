735a9a6291fbe25a4e51fb0ecdfea7b952c8d2b7
ExeLisor <sanin.sanek2019@yandex.ru>
2024-07-07 21:59:34 +0300

    Added firebase_analytics package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:autoverse/exports.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   MobileAds.instance.initialize();
+  await _initFirebase();
+  await _sendAnalyticsEvent();
 
   Get.put(RegistrationController());
   Get.put(EmailController());
@@ -10,6 +15,36 @@ void main() {
   Get.put(HomeScreenController());
 
   runApp(const MainApp());
+}
+
+Future<void> _initFirebase() async {
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+  } catch (e) {
+    log("Failed to initialize Firebase: $e");
+  }
+}
+
+Future<void> _sendAnalyticsEvent() async {
+  try {
+    await FirebaseAnalytics.instance.logEvent(
+      name: 'test_event',
+      parameters: {
+        'string': 'string',
+        'int': 42,
+        'long': 12345678910,
+        'double': 42.0,
+        'bool': true.toString(),
+      },
+    );
+
+    log('logEvent succeeded');
+  } catch (e) {
+    log("Failed to initialize Firebase: $e");
+  }
 }
 
 class MainApp extends StatefulWidget {
