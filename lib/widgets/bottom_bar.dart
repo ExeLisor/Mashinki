573835b9ffefe5c3e667e30e8a0bc84739910d9d
ExeLisor <sanin.sanek2019@yandex.ru>
2024-08-13 11:17:24 +0300

    Changed bottom bar

--- lib/widgets/bottom_bar.dart
+++ lib/widgets/bottom_bar.dart
@@ -6,13 +6,26 @@ class HomeScreenBottomBarWidget extends StatelessWidget {
   final BarController _barController = Get.put(BarController());
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => BottomNavigationBar(
-        showSelectedLabels: false,
-        items: _items(),
-        backgroundColor: Colors.white,
-        currentIndex: _barController.currentPageIndex.value,
-        onTap: _barController.changePage,
+    return Container(
+      height: 77.h,
+      padding: EdgeInsets.symmetric(
+        horizontal: 12.w,
+      ),
+      clipBehavior: Clip.antiAlias,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x33000000),
+            blurRadius: 13,
+            offset: Offset(0, -1),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: _items(),
       ),
     );
   }
@@ -24,19 +37,28 @@ class HomeScreenBottomBarWidget extends StatelessWidget {
     accountCircleIcon
   ];
 
-  List<BottomNavigationBarItem> _items() {
+  List<Widget> _items() {
     return List.generate(
       4,
-      (index) => BottomNavigationBarItem(
-        label: "",
-        backgroundColor: Colors.white,
-        icon: SvgPicture.asset(
-          _icons[index],
-          height: 22.h,
-          width: 22.h,
-          color: _barController.currentPageIndex.value == index
-              ? primaryColor
-              : unactiveColor,
+      (index) => Obx(
+        () => GestureDetector(
+          onTap: () => _barController.currentPageIndex(index),
+          child: Container(
+            height: 70.h,
+            padding: EdgeInsets.symmetric(
+              horizontal: 30.w,
+            ),
+            decoration:
+                BoxDecoration(border: Border.all(color: Colors.transparent)),
+            child: SvgPicture.asset(
+              _icons[index],
+              height: 28.h,
+              width: 28.h,
+              color: _barController.currentPageIndex.value == index
+                  ? primaryColor
+                  : unactiveColor,
+            ),
+          ),
         ),
       ),
     );
