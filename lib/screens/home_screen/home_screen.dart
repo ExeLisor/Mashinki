9dde4076cf2cf34483a52da1104c18b65ff7e3ee
ExeLisor <sanin.sanek2019@yandex.ru>
2024-09-23 14:43:04 +0300

    Added native splash screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -50,7 +50,7 @@ class HomeScreen extends StatelessWidget {
             SizedBox(
               height: 20.h,
             ),
-            MarksWidget(),
+            const MarksWidget(),
             const CarsCatalogListWidget(),
           ],
         ),
